4c5ce3d75e0921daef838ab76fe0ba04ae9f64a6
Sena Oz <tazeyta@gmail.com>
2023-10-13 16:06:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# urla-store
+ 
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss'
+import colors from "tailwindcss/colors";
+
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors: {
+      ...colors,
+      primary: `#141E13`,
+      secondary: `#F1EE8C`,
+      neutral: colors.zinc,
+    }
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+}
+export default config
